4f431c192fb5d7af5b38a6124ca50bd1ccca435e
Auto Deployer <actions@github.com>
2026-09-02 21:47:13 +0000

    Deploy game-tank-duel

--- game-tank-duel/assets/BufferResource-U2NTAmuo.js
+++ game-tank-duel/assets/BufferResource-U2NTAmuo.js
@@ -1,4 +1,4 @@
-import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-CZ45sNwC.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-BSA6gHLx.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
